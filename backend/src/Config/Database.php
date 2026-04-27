5a1c7749780800cf8a83c0cf8432d19c329cd30a
XBilbaoo <x_bilbaozam@fpzornotza.com>
2026-04-27 15:34:50 +0200

    refactorig for deploy

--- backend/src/Config/Database.php
+++ backend/src/Config/Database.php
@@ -7,13 +7,17 @@ class Database
     private $connection;
 
     private function __construct(){
-        $host = getenv('DB_HOST') ?: 'base-datos';
-        $user = getenv('DB_USER') ?: 'root';
-        $password = getenv('DB_PASS') ?: 'root';
-        $dbName = getenv('DB_NAME') ?: 'tfgDAW2526';
+        $host = $_SERVER['DB_HOST'] ?? $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: 'base-datos';
+        $user = $_SERVER['DB_USER'] ?? $_ENV['DB_USER'] ?? getenv('DB_USER') ?: 'root';
+        $password = $_SERVER['DB_PASS'] ?? $_ENV['DB_PASS'] ?? getenv('DB_PASS') ?: 'root';
+        $dbName = $_SERVER['DB_NAME'] ?? $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?: 'tfgDAW2526';
+        // Añadimos el puerto:
+        $port = $_SERVER['DB_PORT'] ?? $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: 3306;
 
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $this->connection = new \mysqli($host, $user, $password, $dbName);
+
+        // Pasamos el puerto como quinto argumento
+        $this->connection = new \mysqli($host, $user, $password, $dbName, (int)$port);
 
         if ($this->connection->connect_error) {
             throw new \Exception("Error: " . $this->connection->connect_error);
